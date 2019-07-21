68d6b59230873aa0821a30062f85b33c6c2b509b
Seeeeeeulki <tmfrl3496@dgist.ac.kr>
2019-07-21 13:08:12 +0900

    thread ver success

--- counter.c
+++ counter.c
@@ -4,7 +4,7 @@
 #include <papi.h>
 #include <pthread.h>
 
-#define PID 1211	
+#define PID 1126	
 
 int fatal(char *s){
 	perror(s);
@@ -19,32 +19,41 @@ typedef struct memcached{
 }mem;
 
 void* thread(void* server){
-	int retval, cycle_, inst_;
+	int cycle_, inst_;
 	float cpi_;
 	//initialize to use PAPI
 	int EventSet = PAPI_NULL;
-	PAPI_library_init(PAPI_VER_CURRENT);
+	int Events[2] = {PAPI_TOT_CYC, PAPI_TOT_INS};
 	PAPI_create_eventset(&EventSet);
-	PAPI_add_event(EventSet, PAPI_TOT_INS);
-	retval = PAPI_attach(EventSet, (unsigned long)(((mem*)server)->pid));
+	PAPI_add_events(EventSet, Events, 2);
+
+
+	//PAPI_add_event(EventSet, PAPI_TOT_CYC);
+	PAPI_attach(EventSet, (unsigned long)(((mem*)server)->pid));
 
 	//PAPI count total cycle, total instruction
-	int Events[2] = {PAPI_TOT_CYC, PAPI_TOT_INS};
 	long long values[2];
-	PAPI_start_counters(Events, 2);
-	printf("hi\n");
-	//sleep(1);
-	PAPI_stop_counters(values, 2);
+
+
+//	long long start, end;
+	PAPI_start(EventSet);
+		
+	//start = PAPI_get_real_cyc();
+	sleep(5);
+	//end = PAPI_get_real_cyc();
+	
+	//cycle_ = end - start;
+	PAPI_stop(EventSet, values);
 	cycle_ = values[0];
 	inst_ = values[1];
-	if(inst_ == 0)
-		cpi_ = -1;
-	else
-		cpi_= (float)cycle_ / (float)inst_;
+	if(inst_ == 0){
+		printf("can not get instruction counter\n");
+		exit(0);
+	}
+	cpi_= (float)cycle_ / (float)inst_;
 	((mem*)server)->cycle = cycle_;
 	((mem*)server)->inst = inst_;
 	((mem*)server)->cpi = cpi_;
-	PAPI_shutdown();
 	pthread_exit(NULL);
 }
 
@@ -56,15 +65,17 @@ void* temp(void* server){
 int main(){
 	mem* server = (mem*)malloc(sizeof(mem)*4);
 	pthread_t tid[4];
-	
+	PAPI_library_init(PAPI_VER_CURRENT);
+
 	for(int i=0; i<4 ; i++){
-//		server[i].pid = PID+i;
+		server[i].pid = PID+i;
 		pthread_create(&tid[i], NULL, thread, (void*)&(server[i]));
 //		pthread_create(&tid[i], NULL, temp, NULL);
 	}
 	for(int i=0; i<4 ; i++){
 		pthread_join(tid[i], NULL);
-		printf("cpi of thread #%d: %f\n", i, server[i].cpi);
+		printf("cpi of pid #%d: %f\n", server[i].pid, server[i].cpi);
+		printf("cycle : %d , inst : %d \n\n", server[i].cycle, server[i].inst);
 	}
 	return 0;
 }
